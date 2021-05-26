ea0623da1abb30c0105946fe252ec09db93bed0a
tobitohikimori <tobitohikimori@gmail.com>
2021-05-26 18:21:18 +0900

    add sbie theme

---
+++ ShopT/ResourceDictionary/SbieTheme.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopT.ResourceDictionary
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SbieTheme
+    {
+        public SbieTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
